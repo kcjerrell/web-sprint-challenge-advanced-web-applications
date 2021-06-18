1a0422c6e383108ad1bcf439dcec1d76ade157fa
Kelly Jerrell <kcjerrell@gmail.com>
2021-06-18 15:32:14 -0700

    changed error text

--- src/components/Login.js
+++ src/components/Login.js
@@ -59,7 +59,7 @@ const Login = () => {
         history.push("/bubblepage")
       })
       .catch((error) => {
-        setError("Username or Password not valid")
+        setError("Username or Password not valid.")
       });
   }
 
